8128b8c4ecb05a902eec315a48865572030fdfab
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-11 04:49:11 -0300

    Ajustando dados

---
+++ 002 - CriandoChatBot/chatbot.py
@@ -0,0 +1,8 @@
+# !pip install -q -u google-generativeai
+# Instalando o SDK do google
+
+# import google.generativeai as genai
+
+# GOOGLE_API_KEY="AIzaSyC0NVEAlxTQNxLEJ9yH_Yyna7KY8u41iHQ"
+# genai.configure(api_key=GOOGLE_API_KEY)
+# Listando os modelos disponiveis.
